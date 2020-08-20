791645789a1c45afc859fd1dbd5bf1293829fd12
wvanzeist <65263211+wvanzeist@users.noreply.github.com>
2020-08-20 16:14:12 +1200

    Function to output data *within* Python
    The first functions saves the data in an external file, which would not be efficient if the user wanted to immediately continue using the data in the same script/Jupyter notebook etc.

--- gwexporter.py
+++ gwexporter.py
@@ -55,3 +55,51 @@ def waveform_exporter(time,freq,amp,path):
         
     #saving exportarray to a file
     np.savetxt(path,exportarray,delimiter='\t',newline='\n')
+
+def waveform_arrayer(time,freq,amp):
+    """
+    Function to collate important data of the simulated gravitational waveform
+    (for SNR calculation etc.) into a single array for ease of storage. Similar
+    to waveform_exporter(), but instead of outputting the area into a file,
+    this function outputs the data inline in Python, in a numpy.ndarray.
+    
+    Parameters
+    ----------
+    time: list of floats
+        The time at each data point. For a BH-BH merger, use i_m_time from
+        time_frequency_stitching in matchingfuns. For a BH-NS or NS-NS merger,
+        use i_time (realtimes) from inspiral_time_conversion in inspiralfuns.
+    freq: list of floats
+        The frequency of the GW signal at each data point. For a BH-BH merger,
+        use i_m_freq from frequency_SI_units in matchingfuns. For a BH-NS or
+        NS-NS merger, use i_freq (freq) from inspiral_phase_freq_integration
+        in inspiralfuns.
+    amp: list of floats
+        The amplitude of the GW strain at each data point. For a BH-BH merger,
+        use i_m_amp from amplitude_stitching in mergersecondfuns. For a BH-NS
+        or NS-NS merger, use i_amp from inspiral_strain_amplitude in
+        inspiralfuns.
+        
+    Results
+    -------
+    exportarray: numpy.ndarray
+        An array containing the important data of the simulated gravitational
+        waveform: the first column is the time, the second is the frequency and
+        the third is the amplitude.
+    """
+    
+    #input type checking
+    for each_variable in locals():
+        assert type(each_variable) == list, 'All inputs should be lists.'
+    
+    #checking the lists all have the same length
+    assert len(time) == len(freq) == len(amp), ('The three lists all need to '
+                                                'have the same length.')
+    
+    exportarray = np.zeros((len(time),3))
+    for i in range(len(time)):
+        exportarray[i,0] = time[i]
+        exportarray[i,1] = freq[i]
+        exportarray[i,2] = amp[i]
+        
+    return exportarray
