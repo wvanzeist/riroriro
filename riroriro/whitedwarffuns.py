7ac9b484b4fcc2c5bcf5dced98bf3e8e7a31ef46
Wouter van Zeist <woutervanzeist7@gmail.com>
2021-03-18 18:34:04 +1300

    Dimensional correction to Shah freq_dot equation
    Based on Królak et al. (2004) equation 21; it appears that Shah equation 4 has a dimensional error.

--- riroriro/whitedwarffuns.py
+++ riroriro/whitedwarffuns.py
@@ -79,8 +79,9 @@ def wd_polarisations(Mc,freq,d,T_sim,init_phase=0.0,chirp=False):
     """
     Calculates the values of the two polarisation of strain for a non-chirping
     or weakly chirping white dwarf binary, based on Shah et al. (2012)
-    equations 1,2,4. Here we treat the inclination as being optimal; effects
-    of inclination are applied by a subsequent function.
+    equations 1,2,4 and Królak et al. (2004) equation 21. Here we treat the
+    inclination as being optimal; effects of inclination are applied by a
+    subsequent function.
     
     Parameters
     ----------
@@ -117,9 +118,23 @@ def wd_polarisations(Mc,freq,d,T_sim,init_phase=0.0,chirp=False):
     
     #basic constants
     pi=np.pi
+    c=2.99792458e8 #m s^-1
+    G=6.674e-11 #m^3 kg^-1 s^-2
     
-    freq_dot = (96/5) * (freq/Mc) * (pi*freq*Mc)**(8/3) #Shah eq. 4
+    #factors for conversion to SI units for convenience
+    Msun_to_kg=1.9885e30
+    
     amp = wd_amplitude(Mc,freq,d)                       #Shah eq. 3
+    
+    if chirp == True:
+        freq_dot = (96/5) * (freq/(Mc*Msun_to_kg)) * (pi*freq * \
+            Mc*Msun_to_kg)**(8/3) * (G/c**3)**(5/3)
+        #Shah eq. 4 with dimensional correction term based on Królak eq. 21
+    else:
+        freq_dot = 0
+        
+    #the freq_dot equation does not need to be integrated to obtain freq as
+    #long as freq >> freq_dot
     
     no_of_samples = int(np.ceil(T_sim * 8*freq))
     #sampling at t=0 Nyquist frequency times 4
