c0038af0d15061e012db9f2ba4f8a0af7c525829
Wouter van Zeist <65263211+wvanzeist@users.noreply.github.com>
2025-06-27 20:21:47 +0200

    Remove deprecated astropy_init
    This resolves issue #25. Based on corresponding fix in Hoki from 2024.

--- riroriro/__init__.py
+++ riroriro/__init__.py
@@ -1,11 +1,5 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
-# Packages may add whatever they like to this file, but
-# should keep this content at the top.
-# ----------------------------------------------------------------------------
-from ._astropy_init import *   # noqa
-# ----------------------------------------------------------------------------
-
 __all__ = []
 """
 from .example_mod import *   # noqa
